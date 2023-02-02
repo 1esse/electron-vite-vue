bbd84a0fe382886e5e51bc0d68e7fe9ed4a4c0be
zhao-jiexin <zhaojiexin@hongjianguo.com>
2023-02-02 17:53:57 +0800

    新增@索引支持

--- renderer/vite.config.ts
+++ renderer/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { join } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: __dirname,
   publicDir: 'public',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
   build: {
     outDir: '../dist/renderer',
     assetsDir: '',
